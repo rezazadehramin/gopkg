d196db99e777420c495cdb85cbb88a5827c489fc
Ramin Rezazadeh <ramram.rezazadeh@gmail.com>
2022-02-23 14:21:37 +0100

    Add retry connection

--- pkg/database/mysql.go
+++ pkg/database/mysql.go
@@ -1,6 +1,9 @@
 package database
 
 import (
+	"fmt"
+	"time"
+
 	"gorm.io/driver/mysql"
 	"gorm.io/gorm"
 )
@@ -11,9 +14,25 @@ type mySQL struct {
 
 // Connect implements the connector interface for mysql.
 func (m *mySQL) Connect() (*gorm.DB, error) {
-	db, err := gorm.Open(mysql.Open(m.Config.DSN()), &gorm.Config{})
-	if err != nil {
-		return nil, err
+	db, _ := gorm.Open(mysql.Open(m.Config.DSN()), &gorm.Config{})
+	sqlDB, _ := db.DB()
+	sqlDB.Ping()
+	retryCount := 10
+
+	for {
+		err := sqlDB.Ping()
+		if err != nil {
+			if retryCount == 0 {
+				return nil, fmt.Errorf("not able to establish connection to database")
+			}
+
+			fmt.Sprintf("Could not connect to database. Wait 1 second. %d retries left...", retryCount)
+			retryCount--
+			time.Sleep(1 * time.Second)
+		} else {
+			break
+		}
 	}
+
 	return db, nil
 }
